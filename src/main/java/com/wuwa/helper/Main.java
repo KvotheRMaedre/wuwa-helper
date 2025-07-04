67d6e935aa9eb2bc30c8a35afb09fd2b57790a46
Ítalo H. <italodssouza.backup@gmail.com>
2025-07-03 22:47:43 -0300

    Material class

--- src/main/java/com/wuwa/helper/Main.java
+++ src/main/java/com/wuwa/helper/Main.java
@@ -1,13 +1,16 @@
 package com.wuwa.helper;
 
-import org.springframework.boot.SpringApplication;
+import com.wuwa.helper.model.Material;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
 
 	public static void main(String[] args) {
-
+		Material lfHowlerCore = new Material();
+		lfHowlerCore.id = 1;
+		lfHowlerCore.name = "LF Howler Core";
+		System.out.println(lfHowlerCore.name);
 	}
 
 }
---
+++ src/main/java/com/wuwa/helper/model/Material.java
@@ -0,0 +1,7 @@
+package com.wuwa.helper.model;
+
+public class Material {
+    public String name;
+    public int id;
+    public Material madeFrom;
+}
