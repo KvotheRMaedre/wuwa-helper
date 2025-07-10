5d00f40eeb8cdad33fc10e9367e80233af2c6164
Ítalo H. <italodssouza.backup@gmail.com>
2025-07-10 17:55:54 -0300

    Creatin routes for getAll, update and delete

--- src/main/java/com/wuwa/helper/controller/WeaponTypeController.java
+++ src/main/java/com/wuwa/helper/controller/WeaponTypeController.java
@@ -1,14 +1,13 @@
 package com.wuwa.helper.controller;
 
-import com.wuwa.helper.dto.UserDTO;
 import com.wuwa.helper.dto.WeaponTypeDTO;
-import com.wuwa.helper.entity.User;
 import com.wuwa.helper.entity.WeaponType;
 import com.wuwa.helper.services.WeaponTypeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/weaponType")
@@ -24,5 +23,43 @@ public class WeaponTypeController {
     public ResponseEntity<WeaponType> createWeaponType(@RequestBody WeaponTypeDTO request){
         var weaponTypeId = weaponTypeService.createWeaponType(request);
         return ResponseEntity.created(URI.create("/v1/weaponType/" + weaponTypeId.toString())).build();
+    }
+
+    @GetMapping({"/{weaponTypeId}"})
+    public ResponseEntity<WeaponType> getWeaponTypeById(@PathVariable String weaponTypeId){
+        try{
+            var weaponType = weaponTypeService.getWeaponTypeById(weaponTypeId);
+            return weaponType.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        } catch (IllegalArgumentException exception){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<WeaponType>> getAllWeaponType(){
+        var weaponTypeList = weaponTypeService.getAllWeaponType();
+        return ResponseEntity.ok(weaponTypeList);
+    }
+
+    @DeleteMapping("{weaponTypeId}")
+    public ResponseEntity<Void> deleteWeaponTypeById(@PathVariable String weaponTypeId){
+        if(weaponTypeService.weaponTypeExists(weaponTypeId)){
+            weaponTypeService.deleteWeaponTypeById(weaponTypeId);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping({"/{weaponTypeId}"})
+    public ResponseEntity<WeaponType> updateWeaponTypeById(@PathVariable String weaponTypeId, @RequestBody WeaponTypeDTO request){
+        try{
+            if (weaponTypeService.weaponTypeExists(weaponTypeId)) {
+                var weaponType = weaponTypeService.updateWeaponType(weaponTypeId,request);
+                return ResponseEntity.ok(weaponType);
+            }
+            return ResponseEntity.notFound().build();
+        } catch (IllegalArgumentException exception){
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/com/wuwa/helper/services/WeaponTypeService.java
+++ src/main/java/com/wuwa/helper/services/WeaponTypeService.java
@@ -6,6 +6,8 @@ import com.wuwa.helper.repository.WeaponTypeRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -26,5 +28,31 @@ public class WeaponTypeService {
         );
         var savedWeaponType = weaponTypeRepository.save(weaponType);
         return savedWeaponType.getId();
+    }
+
+    public Optional<WeaponType> getWeaponTypeById(String weaponTypeId){
+        return weaponTypeRepository.findById(UUID.fromString(weaponTypeId));
+    }
+
+    public List<WeaponType> getAllWeaponType(){
+        return weaponTypeRepository.findAll();
+    }
+
+    public boolean weaponTypeExists(String weaponTypeId){
+        return weaponTypeRepository.existsById(UUID.fromString(weaponTypeId));
+    }
+
+    public void deleteWeaponTypeById(String weaponTypeId){
+        weaponTypeRepository.deleteById(UUID.fromString(weaponTypeId));
+    }
+
+    public WeaponType updateWeaponType(String weaponTypeId, WeaponTypeDTO weaponTypeDTO){
+        var weaponType = weaponTypeRepository.findById(UUID.fromString(weaponTypeId));
+        if(weaponType.isPresent()){
+            var weaponTypeToBeUpdated = weaponType.get();
+            weaponTypeToBeUpdated.setName(weaponTypeDTO.name());
+            return weaponTypeRepository.save(weaponTypeToBeUpdated);
+        }
+        return null;
     }
 }
